8259a43c02a637f9a15ef8c65208d9bab9ace779
Ubuntu <ubuntu@ip-172-31-8-206.eu-west-1.compute.internal>
2018-02-28 17:31:46 +0000

    adding tests

---
+++ tests/sitetest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sleep 5
+if curl http://serve:5000 | grep site/; then
+  echo "Site exists.\nSite test passes.\n"
+  exit 0
+else
+  echo "Site does not exist.\nSite test fails."
+  exit 1
+fi
+
